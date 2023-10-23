42619c3d6ac7ac3a4e46e09f9982ff2448382453
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-10-23 23:49:57 +0300

    first commit

---
+++ build/ros_arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kevo/ros_ws/src/ros_arm/src/ros_arm_description
+# Build directory: /home/kevo/ros_ws/src/ros_arm/build/ros_arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
